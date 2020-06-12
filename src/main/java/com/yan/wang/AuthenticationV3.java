64f41f88477e5634f41c7e31a766fcb76495dac8
Yan Wang <yan.wang.ch@gmail.com>
2020-06-12 10:17:47 +0200

    Run V3

--- src/main/java/com/yan/wang/AuthenticationV3.java
+++ src/main/java/com/yan/wang/AuthenticationV3.java
@@ -6,6 +6,7 @@ import org.apache.commons.codec.binary.Hex;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import java.math.BigDecimal;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -22,8 +23,167 @@ public class AuthenticationV3 {
         public static void main(String[] args) {
             List<UserTransactionPojo> userTransactionPojoList = loadTransactionsFromDB();
             // Deposit
+            computeTotalDeposit(userTransactionPojoList);
+            // Withdrawal
+            computeTotalWithdrawal(userTransactionPojoList);
+            // Sub Account
+            computeSubAccountTotal(userTransactionPojoList);
+            // Market
+            computeBuyAndSellTotal(userTransactionPojoList);
+        }
+
+    public static void computeBuyAndSellTotal(List<UserTransactionPojo> userTransactionPojoList) {
+        BigDecimal totalBuyAndSellUSD = new BigDecimal("0.00");
+        BigDecimal totalBuyAndSellEUR = new BigDecimal("0.00");
+        BigDecimal totalBuyAndSellUSDFee = new BigDecimal("0.00");
+        BigDecimal totalBuyAndSellEURFee = new BigDecimal("0.00");
+
+        for (UserTransactionPojo transactionPojo : userTransactionPojoList) {
+            if (transactionPojo.getType().equals("Market")) {
+                String[] buyAndSellValueTab = transactionPojo.getValue().split(" ");
+                BigDecimal buyAndSellValue = new BigDecimal(buyAndSellValueTab[0]);
+
+                String[] buyAndSellFeeTab;
+                BigDecimal buyAndSellFee;
+                if (transactionPojo.getFee() == null || transactionPojo.getFee().equals("")) {
+                    buyAndSellFee = new BigDecimal("0.00");
+                } else {
+                    buyAndSellFeeTab = transactionPojo.getFee().split(" ");
+                    buyAndSellFee = new BigDecimal(buyAndSellFeeTab[0]);
+                }
+
+                if (buyAndSellValueTab[1].equals("USD")) {
+                    totalBuyAndSellUSDFee = totalBuyAndSellUSDFee.add(buyAndSellFee);
+
+                    if (transactionPojo.getSubtype().equals("Buy")) {
+                        totalBuyAndSellUSD = totalBuyAndSellUSD.subtract(buyAndSellValue);
+                    } else if (transactionPojo.getSubtype().equals("Sell")) {
+                        totalBuyAndSellUSD = totalBuyAndSellUSD.add(buyAndSellValue);
+                    } else {
+                        System.out.println("Weird, operation is not Buy or Sell.");
+                    }
+                } else if (buyAndSellValueTab[1].equals("EUR")) {
+                    totalBuyAndSellEURFee = totalBuyAndSellEURFee.add(buyAndSellFee);
+
+                    if (transactionPojo.getSubtype().equals("Buy")) {
+                        totalBuyAndSellEUR = totalBuyAndSellEUR.subtract(buyAndSellValue);
+                    } else if (transactionPojo.getSubtype().equals("Sell")) {
+                        totalBuyAndSellEUR = totalBuyAndSellEUR.add(buyAndSellValue);
+                    } else {
+                        System.out.println("Weird, operation is not Buy or Sell.");
+                    }
+                } else {
+                    System.out.println("Weird, currency is not USD or EUR.");
+                }
+            }
+        }
+        System.out.println("Total Buy and Sell USD = " + totalBuyAndSellUSD + " USD");
+        System.out.println("Total Buy and Sell Fee USD = " + totalBuyAndSellUSDFee + " USD");
+        System.out.println("Total Buy and Sell EUR = " + totalBuyAndSellEUR + " EUR");
+        System.out.println("Total Buy and Sell Fee EUR = " + totalBuyAndSellEURFee + " EUR");
+    }
 
 
+    public static void computeSubAccountTotal(List<UserTransactionPojo> userTransactionPojoList) {
+        BigDecimal totalSubAccountTransferUSD = new BigDecimal("0.00");
+        BigDecimal totalSubAccountTransferEUR = new BigDecimal("0.00");
+        BigDecimal totalSubAccountTransferBTC = new BigDecimal("0.00");
+
+        for (UserTransactionPojo transactionPojo : userTransactionPojoList) {
+            if (transactionPojo.getType().equals("Sub Account Transfer")) {
+                String[] subAccountTransferAmountTab = transactionPojo.getAmount().split(" ");
+                BigDecimal subAccountTransferAmount = new BigDecimal(subAccountTransferAmountTab[0]);
+
+                if (subAccountTransferAmountTab[1].equals("USD")) {
+                    if (transactionPojo.getValue().equals("Addition")) {
+                        totalSubAccountTransferUSD = totalSubAccountTransferUSD.add(subAccountTransferAmount);
+                    } else if (transactionPojo.getValue().equals("Subtraction")) {
+                        totalSubAccountTransferUSD = totalSubAccountTransferUSD.subtract(subAccountTransferAmount);
+                    } else {
+                        System.out.println("Weird, Operation is not Addition or Subtraction.");
+                    }
+                } else if (subAccountTransferAmountTab[1].equals("EUR")) {
+                    if (transactionPojo.getValue().equals("Addition")) {
+                        totalSubAccountTransferEUR = totalSubAccountTransferEUR.add(subAccountTransferAmount);
+                    } else if (transactionPojo.getValue().equals("Subtraction")) {
+                        totalSubAccountTransferEUR = totalSubAccountTransferEUR.subtract(subAccountTransferAmount);
+                    } else {
+                        System.out.println("Weird, Operation is not Addition or Subtraction.");
+                    }
+                } else if (subAccountTransferAmountTab[1].equals("BTC")) {
+                    if (transactionPojo.getValue().equals("Addition")) {
+                        totalSubAccountTransferBTC = totalSubAccountTransferBTC.add(subAccountTransferAmount);
+                    } else if (transactionPojo.getValue().equals("Subtraction")) {
+                        totalSubAccountTransferBTC = totalSubAccountTransferBTC.subtract(subAccountTransferAmount);
+                    } else {
+                        System.out.println("Weird, Operation is not Addition or Subtraction.");
+                    }
+                } else {
+                    System.out.println("Weird, currency is not USD or EUR.");
+                }
+            }
+        }
+
+        if (totalSubAccountTransferUSD.compareTo(BigDecimal.ZERO) != 0) {
+            System.out.println("Total Sub Account Transfer USD = " + totalSubAccountTransferUSD + " USD");
+        }
+        if (totalSubAccountTransferEUR.compareTo(BigDecimal.ZERO) != 0) {
+            System.out.println("Total Sub Account Transfer USD = " + totalSubAccountTransferEUR + " EUR");
+        }
+        if (totalSubAccountTransferBTC.compareTo(BigDecimal.ZERO) != 0) {
+            System.out.println("Total Sub Account Transfer BTC = " + totalSubAccountTransferBTC + " BTC");
+        }
+    }
+
+    public static void computeTotalWithdrawal(List<UserTransactionPojo> userTransactionPojoList) {
+        BigDecimal totalWithdrawalUSD = new BigDecimal("0.00");
+        BigDecimal totalWithdrawalEUR = new BigDecimal("0.00");
+        BigDecimal totalWithdrawalUSDFee = new BigDecimal("0.00");
+        BigDecimal totalWithdrawalEURFee = new BigDecimal("0.00");
+
+        for (UserTransactionPojo transactionPojo : userTransactionPojoList) {
+            if (transactionPojo.getType().equals("Withdrawal")) {
+                String[] withdrawalAmountTab = transactionPojo.getAmount().split(" ");
+                BigDecimal depositAmount = new BigDecimal(withdrawalAmountTab[0]);
+
+                String[] withdarawalFeeTab = transactionPojo.getFee().split(" ");
+                BigDecimal depositFee = new BigDecimal(withdarawalFeeTab[0]);
+
+                if (withdrawalAmountTab[1].equals("USD")) {
+                    totalWithdrawalUSD = totalWithdrawalUSD.add(depositAmount);
+                    totalWithdrawalUSDFee = totalWithdrawalUSDFee.add(depositFee);
+                } else if (withdrawalAmountTab[1].equals("EUR")) {
+                    totalWithdrawalEUR = totalWithdrawalEUR.add(depositAmount);
+                    totalWithdrawalEURFee = totalWithdrawalEURFee.add(depositFee);
+                } else {
+                    System.out.println("Weird, currency is not USD or EUR.");
+                }
+            }
+        }
+        System.out.println("Total Withdrawal USD = " + totalWithdrawalUSD + " USD");
+        System.out.println("Total Withdrawal Fee USD = " + totalWithdrawalUSDFee + " USD");
+        System.out.println("Total Withdrawal EUR = " + totalWithdrawalEUR + " EUR");
+        System.out.println("Total Withdrawal Fee EUR = " + totalWithdrawalEURFee + " EUR");
+    }
+
+        public static void computeTotalDeposit(List<UserTransactionPojo> userTransactionPojoList) {
+            BigDecimal totalDepositUSD = new BigDecimal("0.00");
+            BigDecimal totalDepositEUR = new BigDecimal("0.00");
+            for (UserTransactionPojo transactionPojo : userTransactionPojoList) {
+                if (transactionPojo.getType().equals("Deposit")) {
+                    String[] depositAmountTab = transactionPojo.getAmount().split(" ");
+                    BigDecimal depositAmount = new BigDecimal(depositAmountTab[0]);
+                    if (depositAmountTab[1].equals("USD")) {
+                        totalDepositUSD = totalDepositUSD.add(depositAmount);
+                    } else if (depositAmountTab[1].equals("EUR")) {
+                        totalDepositEUR = totalDepositEUR.add(depositAmount);
+                    } else {
+                        System.out.println("Weird, currency is not USD or EUR.");
+                    }
+                }
+            }
+            System.out.println("Total Deposit USD = " + totalDepositUSD + " USD");
+            System.out.println("Total Deposit EUR = " + totalDepositEUR + " EUR");
         }
 
         public static List<UserTransactionPojo> loadTransactionsFromDB() {
